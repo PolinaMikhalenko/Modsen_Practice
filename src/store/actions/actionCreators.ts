cebd84a3372f06499b9e0887f06972db3a9e4226
PolinaMikhalenko <73405361+PolinaMikhalenko@users.noreply.github.com>
2023-04-11 23:58:42 +0300

    update actions

--- src/store/actions/actionCreators.ts
+++ src/store/actions/actionCreators.ts
@@ -1,12 +1,21 @@
 import { createAction } from '@reduxjs/toolkit';
+import {
+  IDailyForecastApiResponse,
+  IHourlyForecastApiResponse,
+} from '../../interfaces/IForecastApiResponse';
 
-export const getDailyWeatherForecast = createAction(
-  'WEATHER/GET_DAILY_FORECAST'
-);
+export const getDailyWeatherForecastFulfilled =
+  createAction<IDailyForecastApiResponse>(
+    'WEATHER/GET_DAILY_FORECAST_FULFILLED'
+  );
 
-export const getHourlyWeatherForecast = createAction(
-  'WEATHER/GET_HOURLY_FORECAST'
-);
+export const getDailyWeatherForecastPending = createAction<{
+  latitude: number;
+  longitude: number;
+}>('WEATHER/GET_DAILY_FORECAST_PENDING');
+
+export const getHourlyWeatherForecastFulfilled =
+  createAction<IHourlyForecastApiResponse>('WEATHER/GET_HOURLY_FORECAST');
 
 export const getSelectedLocation = createAction(
   'LOCATION/GET_SELECTED_LOCATION'
