f530ba9d12cf7a5f4ddf9fddf77a0fda03594465
PolinaMikhalenko <73405361+PolinaMikhalenko@users.noreply.github.com>
2023-04-11 23:53:53 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import GlobalStyle from './styles/globalStyles';
 import { baseTheme } from './styles/theme';
 import { StyledFlexRow, StyledScrollSection } from './styles/components';
@@ -7,8 +8,14 @@ import { AppLayout } from './layouts/AppLayout';
 import { CurrentDateTime } from './components/CurrentDateTime';
 import { CitySelector } from './components/CitySelector';
 import { WeatherForecast } from './components/WeatherForecast';
+import { getDailyWeatherForecastPending } from './store/actions/actionCreators';
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(
+    getDailyWeatherForecastPending({ latitude: 53.9168, longitude: 30.3449 })
+  );
+
   return (
     <ThemeProvider theme={baseTheme}>
       <GlobalStyle />
