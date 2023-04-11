c7d800fa6edb4239fd6fce0e2c524c949a5b3676
PolinaMikhalenko <73405361+PolinaMikhalenko@users.noreply.github.com>
2023-04-11 23:43:50 +0300

    update components DailyForecast and HourlyForecast

--- src/components/WeatherForecast/DailyForecast/index.tsx
+++ src/components/WeatherForecast/DailyForecast/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { DayWeatherInfo } from './DayWeatherInfo';
+import { nanoid } from '@reduxjs/toolkit';
+import { WeatherInfo } from '../WeatherInfo/index';
 import { RootState } from '../../../store';
 import { StyledForecastSection } from '../../../styles/components';
+import { selectDailyWeatherForecast } from '../../../store/selectors/dailyWeatherForecastSelector';
 
 export function DailyForecast() {
-  const daysWeather = useSelector(
-    (state: RootState) => state.weather.daysWeather
+  const daysWeather = useSelector((state: RootState) =>
+    selectDailyWeatherForecast(state)
   );
   return (
     <StyledForecastSection>
       {daysWeather.map((item) => (
-        <DayWeatherInfo
-          weekDay={item.weekDay}
+        <WeatherInfo
+          timePeriod={item.weekDay}
           icon={item.icon}
+          weatherCondition={item.weatherCondition}
           temperature={item.temperature}
-          key={item.weekDay}
+          key={nanoid()}
         />
       ))}
     </StyledForecastSection>
--- src/components/WeatherForecast/HourlyForecast/index.tsx
+++ src/components/WeatherForecast/HourlyForecast/index.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
-import { HourWeatherInfo } from './HourWeatherInfo';
+import { WeatherInfo } from '../WeatherInfo/index';
 import { StyledForecastSection } from '../../../styles/components';
 
 export function HourlyForecast() {
   return (
     <StyledForecastSection light>
-      <HourWeatherInfo time="12.00" temperature={20} />
-      <HourWeatherInfo time="12.00" temperature={20} />
-      <HourWeatherInfo time="12.00" temperature={20} />
-      <HourWeatherInfo time="12.00" temperature={20} />
-      <HourWeatherInfo time="12.00" temperature={20} />
-      <HourWeatherInfo time="12.00" temperature={20} />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
+      <WeatherInfo
+        timePeriod="12.00"
+        icon=""
+        weatherCondition=""
+        temperature={20}
+      />
     </StyledForecastSection>
   );
 }
