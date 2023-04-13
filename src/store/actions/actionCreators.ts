c7fbb31037689d1f72a18798ad93f3bc62b89f49
PolinaMikhalenko <73405361+PolinaMikhalenko@users.noreply.github.com>
2023-04-13 23:33:10 +0300

    update actionCreators.ts

--- src/store/actions/actionCreators.ts
+++ src/store/actions/actionCreators.ts
@@ -4,18 +4,25 @@ import {
   IHourlyForecastApiResponse,
 } from '../../interfaces/IForecastApiResponse';
 
-export const getDailyWeatherForecastFulfilled =
-  createAction<IDailyForecastApiResponse>(
-    'WEATHER/GET_DAILY_FORECAST_FULFILLED'
-  );
-
 export const getDailyWeatherForecastPending = createAction<{
   latitude: number;
   longitude: number;
 }>('WEATHER/GET_DAILY_FORECAST_PENDING');
 
+export const getDailyWeatherForecastFulfilled =
+  createAction<IDailyForecastApiResponse>(
+    'WEATHER/GET_DAILY_FORECAST_FULFILLED'
+  );
+
+export const getHourlyWeatherForecastPending = createAction<{
+  latitude: number;
+  longitude: number;
+}>('WEATHER/GET_HOURLY_FORECAST_PENDING');
+
 export const getHourlyWeatherForecastFulfilled =
-  createAction<IHourlyForecastApiResponse>('WEATHER/GET_HOURLY_FORECAST');
+  createAction<IHourlyForecastApiResponse>(
+    'WEATHER/GET_HOURLY_FORECAST_FULFILLED'
+  );
 
 export const getSelectedLocation = createAction(
   'LOCATION/GET_SELECTED_LOCATION'
