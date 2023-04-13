1f0ab29b90299889452a094e051876c82654874a
PolinaMikhalenko <73405361+PolinaMikhalenko@users.noreply.github.com>
2023-04-13 23:29:27 +0300

    add styled.ts

---
+++ src/components/WeatherForecast/HourlyForecast/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const HourlyWeatherOfDay = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-top: 1.5rem;
+`;
+
+export const HourlyWeatherSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1.5rem 0;
+`;
