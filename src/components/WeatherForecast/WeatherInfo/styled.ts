8602e66b2fa6f4647718a07ba60eaf62001c05b5
PolinaMikhalenko <73405361+PolinaMikhalenko@users.noreply.github.com>
2023-04-17 19:54:39 +0300

    add styled.ts

---
+++ src/components/WeatherForecast/WeatherInfo/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const WeatherInfo = styled.div`
+  height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 1.3rem;
+  text-align: center;
+  border-radius: 1rem;
+`;
